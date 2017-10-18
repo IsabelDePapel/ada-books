98773146860cd0875b59f4f41db26a71b7cf9028
IsabelDePapel <isuchanek@gmail.com>
2017-10-18 09:41:29 -0700

    turn on oauth test mode and set up user fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,5 +19,13 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # turn on mocking
+  def setup
+    # when test mode enabled, all requests to omniauth will be short-circuited
+    # to use the mock authentication hash
+    OmniAuth.config.test_mode = true
+
+  end
   # Add more helper methods to be used by all tests here...
 end
